53008f41e534034d2e90b61c4c72836f0bac7c5b
Austin Mangelson <austinw.mangelson@gmail.com>
2024-06-15 08:51:41 -0600

    completing task 5

--- Case Study 2.R
+++ Case Study 2.R
@@ -4,15 +4,14 @@ library(dplyr)
 view(gapminder)
 
 
-# create 1st scatter plot -------------------------------------------------
-
-gapminder_without_Kuwait <- 
+#filter out Kuwait and add "population per 100k" column
+gapminder2 <- 
   gapminder |>
-    filter(!country == "Kuwait") |> 
-    mutate(gapminder_without_Kuwait,
-           pop_per_100k = pop / 100000)
+    filter(country != "Kuwait") |> 
+    mutate(pop_per_100k = pop / 100000)
 
-ggplot(gapminder_without_Kuwait, aes(x = lifeExp, y = gdpPercap)) +
+#creating life expectancy vs GDP per capita by year graph
+ggplot(gapminder2, aes(x = lifeExp, y = gdpPercap)) +
   geom_point(aes(
     x = lifeExp, 
     y = gdpPercap,
@@ -23,36 +22,48 @@ ggplot(gapminder_without_Kuwait, aes(x = lifeExp, y = gdpPercap)) +
   theme_bw() +
   scale_y_continuous(trans = "sqrt") +
   labs(
-    title = "Life Expectancy vs. GDP per Capita by Year",
+    title = "Life Expectancy and GDP per capita are positively correlated",
     x = "Life Expectancy", 
     y = "GDP per capita",
     size = "Population (100k)",
     color = "Continent"
   )
 
-# create 2nd line graph ---------------------------------------------------
+#create a weighted mean on the gdp
+weighted_data <- 
+  gapminder2 |> 
+  group_by(continent, year) |> 
+  summarise(weighted_datamean = weighted.mean(gdpPercap, pop, na.rm = TRUE))
+view(weighted_data)
 
-gapminder_without_Kuwait <- 
-  gapminder |>
-  filter(!country == "Kuwait") |> 
-  mutate(gapminder_without_Kuwait,
-         pop_per_100k = pop / 100000)
-
-gapminder_without_Kuwait |> 
-  group_by(continent) |> 
-  summarise(avg_gdp_percap = weighted.mean(gdpPercap))
-
-ggplot(gapminder_without_Kuwait, aes(x = year, y = gdpPercap)) + 
-  geom_line(aes(
+#creating GDP per year growth faceted by continent
+ggplot(gapminder2, aes(x = year, y = gdpPercap)) + 
+  geom_line(gapminder2, aes(
+    x = year,
+    y = gdpPercap,
+    color = continent, 
+    group = country), 
+    linewidth = 0.1) +
+  geom_point(gapminder2, aes(
     x = year, 
     y = gdpPercap, 
     color = continent, 
-  )) +
-  facet_grid(~continent) + 
+    group = country),
+    size = 0.5) +
+  geom_line(data = weighted_data, aes(
+    x = year,
+    y = weighted_datamean),
+    color = "black") +
+  geom_point(data = weighted_data, aes(
+    x = year,
+    y = weighted_datamean),
+    color = "black",
+    size = 1.5) +
+  scale_y_continuous(trans = "sqrt") +
+  facet_wrap(~continent, ncol = 5) + 
   theme_bw() + 
   labs(
     title = "GDP Growth per Year by Continent",
     x = "Year",
     y = "GDP per capita",
-    color = "Continent"
-  )
+    color = "Continent")
